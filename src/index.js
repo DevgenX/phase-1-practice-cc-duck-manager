a24a4dea0db93b2ab23b78de1543893c392700df
devgenx <sebastian110420@gmail.com>
2023-01-03 21:40:26 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-// write your code here!
-
 const getDuckLikes = document.querySelector("#duck-display-likes");
 const getDuckImg = document.querySelector("#duck-display-image");
 const getDuckName = document.querySelector("#duck-display-name");
@@ -7,9 +5,7 @@ const getDuckNav = document.querySelector("#duck-nav");
 
 const fetchData = async () => {
   const response = await fetch("http://localhost:3000/ducks");
-
   const data = await response.json();
-
   data.forEach((duck) => displayDuck(duck));
   data.forEach((duck) => duckNav(duck));
 };
@@ -27,31 +23,26 @@ getDuckLikes.addEventListener("click", () => {
 const duckNav = (data) => {
   const duckImg = document.createElement("img");
   duckImg.src = data.img_url;
-
   duckImg.addEventListener("click", () => {
     displayDuck(data);
   });
-
   getDuckNav.appendChild(duckImg);
 };
 
 const duckForm = document.getElementById("new-duck-form");
-
 duckForm.addEventListener("submit", (e) => {
   e.preventDefault();
-
   const name = document.getElementById("new-name").value;
   const img_url = document.getElementById("new-image").value;
 
   const duck = {
     name,
     img_url,
+    likes: 0,
   };
 
   displayDuck(duck);
-
-  getDuckLikes.textContent = 0;
-
+  duckNav(duck);
   duckForm.reset();
 });
 
